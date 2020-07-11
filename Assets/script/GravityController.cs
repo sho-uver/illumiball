a9eb3f14f7de73a9af57863419a8d5d31db19418
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-07-11 23:00:56 +0900

    重力を制御して球体を動かすスクリプトの実装。

---
+++ Assets/script/GravityController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour {
+    const float Gravity = 9.8f;
+    public float gravityScale = 1.0f;
+    void Update () {
+        Vector3 vector = new Vector3 ();
+        if (Application.isMobilePlatform) {
+            vector.x = Input.acceleration.x;
+            vector.y = Input.acceleration.y;
+            vector.z = input.acceleration.z;
+        } else {
+
+            vector.x = Input.GetAxis ("Horizontal");
+            vector.z = Input.GetAxis ("Vertical");
+            if (Input.GetKey ("z")) {
+                vector.y = 1.0f;
+            } else {
+                vector.y = -1.0f;
+            }
+        }
+        Physics.gravity = Gravity * vector.normalized * gravityScale;
+    }
+}
