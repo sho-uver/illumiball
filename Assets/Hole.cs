0ea652fc3ff7cc5b187a766d2ab52daa06e235e7
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-07-12 22:14:31 +0900

    完成

---
+++ Assets/Hole.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+    public string targetTag;
+    bool isHolding;
+
+    public bool IsHolding () {
+        return isHolding;
+    }
+
+    void OnTriggerEnter (Collider other) {
+        if (other.gameObject.tag == targetTag) {
+            isHolding = true;
+        }
+    }
+    void OnTriggerExit (Collider other) {
+        if (other.gameObject.tag == targetTag) {
+            isHolding = false;
+        }
+    }
+
+    void OnTriggerStay (Collider other) {
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody> ();
+        Vector3 direction = other.gameObject.transform.position - transform.position;
+        direction.Normalize ();
+        if (other.gameObject.tag == targetTag) {
+            r.velocity *= 0.9f;
+            r.AddForce (direction * -20.0f, ForceMode.Acceleration);
+        }
+    }
+}
